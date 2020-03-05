d91e08b545122c3383f36bc0e4c894d9eb68afa0
Rana-Siroosian <rana.siroosian@gmail.com>
2020-03-05 10:48:41 -0500

    Let's get started

---
+++ src/test/java/co/grandcircus/RecipeApi/RecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.RecipeApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
